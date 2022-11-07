8e23f98d76676f06331f6138b7c50756faf0c47e
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-11-07 10:35:29 +0600

    final-code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:bmi_calculator/constants/app_constants.dart';
 import 'package:bmi_calculator/screen/home.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -12,12 +15,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "BMI Calculator",
-      theme: ThemeData(
-          primarySwatch: Colors.yellow,
-          visualDensity: VisualDensity.adaptivePlatformDensity),
-      home: SafeArea(child: HomeScreen()),
-    );
+        debugShowCheckedModeBanner: false,
+        title: "BMI Calculator",
+        theme: ThemeData(
+            primarySwatch: Colors.yellow,
+            visualDensity: VisualDensity.adaptivePlatformDensity),
+        home: AnimatedSplashScreen(
+            duration: 3000,
+            splashIconSize: 300,
+            splash: Image.network("https://play-lh.googleusercontent.com/bE5yyy3pCAi0IpUTbB_xqsSwsvukYUelGzrU3uzS1TRtFr_wpauqLNvbrHa01hZ6N10",),
+            nextScreen: HomeScreen(),
+            splashTransition: SplashTransition.fadeTransition,
+            // pageTransitionType: PageTransitionType.scale,
+            backgroundColor: mainHexColor));
   }
 }
